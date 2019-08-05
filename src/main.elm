1352ead334ae08d8834c03a468cb5322166a9266
floyd33n <com.alife.floyd.e79fa5@gmail.com>
2019-08-05 21:46:31 +0900

    update

--- src/main.elm
+++ src/main.elm
@@ -1,10 +1,10 @@
 module Main exposing(main)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing(..)
 import Html.Events exposing (..)
 import Array exposing (..)
-import List.Extra
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
@@ -12,11 +12,12 @@ main =
 --MODEL--
 type alias Model = 
   { campus : Campus
+  , colorValue : String
   }
 
 init : Model
 init =
-    Model initCampus
+    Model initCampus ""
 
 type alias Campus
     = List (Int, String)
@@ -28,13 +29,17 @@ initCampus =
 --UPDATE--
 type Msg
     = ChangeColor Int String
+    | ColorValue String
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         ChangeColor n color ->
-            { model | campus = updateCampus model n color }
+            { model | campus = updateCampus model n color}
 
+        ColorValue value ->
+            { model | colorValue = value }
+            
 
 --VIEW--
 view : Model -> Html Msg
@@ -42,52 +47,105 @@ view model =
     div []
         [ h1 [] [ text "Pour-Masu" ]
         , p [] [ text "Pixel Art Editor with Elm" ]
-        , table [] [ tr [] [ td [ (onClick (ChangeColor 0 "red")), (style "background-color" (getCampusColor model 0)) ] []
-                           , td [ (onClick (ChangeColor 1 "pink")), (style "background-color" (getCampusColor model 1)) ] []
-                           ]
-                   , tr [] [ td [ (onClick (ChangeColor 2 "green")), (style "background-color" (getCampusColor model 2)) ] []
-                           , td [ (onClick (ChangeColor 3 "blue")), style "background-color" (getCampusColor model 3) ] []
-                           ]
-                   ]
-        , chkFunc model
+        , div [] [ input [ (placeholder "Color"), (onInput ColorValue) ] [] ]
+        , create8x8Campus model
         ]
 
 --FUNC--
 getCampusColor : Model -> Int -> String
 getCampusColor model n =
     Tuple.second (Maybe.withDefault (-1, "_") (Array.get n (Array.fromList model.campus)))
-{-
-updateCampus : Model ->  Int -> String -> List (Int, String)
-updateCampus model n color =
-    let
-        firstTemp : List (Int, String) -> List (Int, String)
-        firstTemp temp =
-            List.take (n-1) model.campus
 
-        updateTemp : Int -> String -> List (Int, String)
-        updateTemp tempn tempcolor =
-            List.singleton ((n-1), color)
+getCampusInt : Model -> Int -> Int
+getCampusInt model n =
+    Tuple.first (Maybe.withDefault (1, "_") (Array.get n (Array.fromList model.campus)))
 
-        secondTemp : List (Int, String) -> List (Int, String)
-        secondTemp temp =
-            List.drop n model.campus
+makeTable : Model -> Int -> Int -> Html Msg
+makeTable model width height =
+    div []
+        [ table []
+            <| List.map(\n -> tr[]
+                <| List.map(\m -> td [] [])
+                    <| List.range 1 width)
+                        <| List.range 1 height ]
 
-
-        appendTemps : List (Int, String) -> List (Int, String)
-        appendTemps temp =
-            List.append (List.append (firstTemp model.campus) (updateTemp n color)) (secondTemp model.campus)
-    in
-        model.campus |> appendTemps
--}
 updateCampus : Model -> Int -> String -> List (Int, String)
 updateCampus model n color =
     List.append (List.append (List.take n model.campus) (List.singleton (n, color))) (List.drop (n+1) model.campus)
 
---DEBUG--
-chkFunc : Model -> Html Msg
-chkFunc model =
-    div []
-        [ h3 [] [ text "--Debug--" ]
-        , button [ onClick (ChangeColor 3 "red")  ] [ text "red" ]
-        , p [ style "color" (getCampusColor model 2) ] [ text "What Color" ] 
+create8x8Campus : Model -> Html Msg
+create8x8Campus model =
+    table []
+        [ tr [] [ td [ onClick (ChangeColor 0 model.colorValue), style "background-color" (getCampusColor model 0) ] []
+                , td [ onClick (ChangeColor 1 model.colorValue), style "background-color" (getCampusColor model 1) ] []
+                , td [ onClick (ChangeColor 2 model.colorValue), style "background-color" (getCampusColor model 2) ] []
+                , td [ onClick (ChangeColor 3 model.colorValue), style "background-color" (getCampusColor model 3) ] []
+                , td [ onClick (ChangeColor 4 model.colorValue), style "background-color" (getCampusColor model 4) ] []
+                , td [ onClick (ChangeColor 5 model.colorValue), style "background-color" (getCampusColor model 5) ] []
+                , td [ onClick (ChangeColor 6 model.colorValue), style "background-color" (getCampusColor model 6) ] []
+                , td [ onClick (ChangeColor 7 model.colorValue), style "background-color" (getCampusColor model 7) ] []
+                ]
+        , tr [] [ td [ onClick (ChangeColor 8 model.colorValue), style "background-color" (getCampusColor model 8) ] []
+                , td [ onClick (ChangeColor 9 model.colorValue), style "background-color" (getCampusColor model 9) ] []
+                , td [ onClick (ChangeColor 10 model.colorValue), style "background-color" (getCampusColor model 10) ] []
+                , td [ onClick (ChangeColor 11 model.colorValue), style "background-color" (getCampusColor model 11) ] []
+                , td [ onClick (ChangeColor 12 model.colorValue), style "background-color" (getCampusColor model 12) ] []
+                , td [ onClick (ChangeColor 13 model.colorValue), style "background-color" (getCampusColor model 13) ] []
+                , td [ onClick (ChangeColor 14 model.colorValue), style "background-color" (getCampusColor model 14) ] []
+                , td [ onClick (ChangeColor 15 model.colorValue), style "background-color" (getCampusColor model 15) ] []
+                ]
+        , tr [] [ td [ onClick (ChangeColor 16 model.colorValue), style "background-color" (getCampusColor model 16) ] []
+                , td [ onClick (ChangeColor 17 model.colorValue), style "background-color" (getCampusColor model 17) ] []
+                , td [ onClick (ChangeColor 18 model.colorValue), style "background-color" (getCampusColor model 18) ] []
+                , td [ onClick (ChangeColor 19 model.colorValue), style "background-color" (getCampusColor model 19) ] []
+                , td [ onClick (ChangeColor 20 model.colorValue), style "background-color" (getCampusColor model 20) ] []
+                , td [ onClick (ChangeColor 21 model.colorValue), style "background-color" (getCampusColor model 21) ] []
+                , td [ onClick (ChangeColor 22 model.colorValue), style "background-color" (getCampusColor model 22) ] []
+                , td [ onClick (ChangeColor 23 model.colorValue), style "background-color" (getCampusColor model 23) ] []
+                ]
+        , tr [] [ td [ onClick (ChangeColor 24 model.colorValue), style "background-color" (getCampusColor model 24) ] []
+                , td [ onClick (ChangeColor 25 model.colorValue), style "background-color" (getCampusColor model 25) ] []
+                , td [ onClick (ChangeColor 26 model.colorValue), style "background-color" (getCampusColor model 26) ] []
+                , td [ onClick (ChangeColor 27 model.colorValue), style "background-color" (getCampusColor model 27) ] []
+                , td [ onClick (ChangeColor 28 model.colorValue), style "background-color" (getCampusColor model 28) ] []
+                , td [ onClick (ChangeColor 29 model.colorValue), style "background-color" (getCampusColor model 29) ] []
+                , td [ onClick (ChangeColor 30 model.colorValue), style "background-color" (getCampusColor model 30) ] []
+                , td [ onClick (ChangeColor 31 model.colorValue), style "background-color" (getCampusColor model 31) ] []
+              ]
+        , tr [] [ td [ onClick (ChangeColor 32 model.colorValue), style "background-color" (getCampusColor model 32) ] []
+                , td [ onClick (ChangeColor 33 model.colorValue), style "background-color" (getCampusColor model 33) ] []
+                , td [ onClick (ChangeColor 34 model.colorValue), style "background-color" (getCampusColor model 34) ] []
+                , td [ onClick (ChangeColor 35 model.colorValue), style "background-color" (getCampusColor model 35) ] []
+                , td [ onClick (ChangeColor 36 model.colorValue), style "background-color" (getCampusColor model 36) ] []
+                , td [ onClick (ChangeColor 37 model.colorValue), style "background-color" (getCampusColor model 37) ] []
+                , td [ onClick (ChangeColor 38 model.colorValue), style "background-color" (getCampusColor model 38) ] []
+                , td [ onClick (ChangeColor 39 model.colorValue), style "background-color" (getCampusColor model 39) ] []
+                ]
+        , tr [] [ td [ onClick (ChangeColor 40 model.colorValue), style "background-color" (getCampusColor model 40) ] []
+                , td [ onClick (ChangeColor 41 model.colorValue), style "background-color" (getCampusColor model 41) ] []
+                , td [ onClick (ChangeColor 42 model.colorValue), style "background-color" (getCampusColor model 42) ] []
+                , td [ onClick (ChangeColor 43 model.colorValue), style "background-color" (getCampusColor model 43) ] []
+                , td [ onClick (ChangeColor 44 model.colorValue), style "background-color" (getCampusColor model 44) ] []
+                , td [ onClick (ChangeColor 45 model.colorValue), style "background-color" (getCampusColor model 45) ] []
+                , td [ onClick (ChangeColor 46 model.colorValue), style "background-color" (getCampusColor model 46) ] []
+                , td [ onClick (ChangeColor 47 model.colorValue), style "background-color" (getCampusColor model 47) ] []
+                ]
+        , tr [] [ td [ onClick (ChangeColor 48 model.colorValue), style "background-color" (getCampusColor model 48) ] []
+                , td [ onClick (ChangeColor 49 model.colorValue), style "background-color" (getCampusColor model 49) ] []
+                , td [ onClick (ChangeColor 50 model.colorValue), style "background-color" (getCampusColor model 50) ] []
+                , td [ onClick (ChangeColor 51 model.colorValue), style "background-color" (getCampusColor model 51) ] []
+                , td [ onClick (ChangeColor 52 model.colorValue), style "background-color" (getCampusColor model 52) ] []
+                , td [ onClick (ChangeColor 53 model.colorValue), style "background-color" (getCampusColor model 53) ] []
+                , td [ onClick (ChangeColor 54 model.colorValue), style "background-color" (getCampusColor model 54) ] []
+                , td [ onClick (ChangeColor 55 model.colorValue), style "background-color" (getCampusColor model 55) ] []
+                ]
+        , tr [] [ td [ onClick (ChangeColor 56 model.colorValue), style "background-color" (getCampusColor model 56) ] []
+                , td [ onClick (ChangeColor 57 model.colorValue), style "background-color" (getCampusColor model 57) ] []
+                , td [ onClick (ChangeColor 58 model.colorValue), style "background-color" (getCampusColor model 58) ] []
+                , td [ onClick (ChangeColor 59 model.colorValue), style "background-color" (getCampusColor model 59) ] []
+                , td [ onClick (ChangeColor 60 model.colorValue), style "background-color" (getCampusColor model 60) ] []
+                , td [ onClick (ChangeColor 61 model.colorValue), style "background-color" (getCampusColor model 61) ] []
+                , td [ onClick (ChangeColor 62 model.colorValue), style "background-color" (getCampusColor model 62) ] []
+                , td [ onClick (ChangeColor 63 model.colorValue), style "background-color" (getCampusColor model 63) ] []
+                ]
         ]
