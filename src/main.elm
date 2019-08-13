61b56d716d9600855d7bd973b0e6293733471b2c
floyd33n <com.alife.floyd.e79fa5@gmail.com>
2019-08-14 00:07:12 +0900

    delete debug

--- src/main.elm
+++ src/main.elm
@@ -5,7 +5,6 @@ import Html exposing (..)
 import Html.Attributes exposing(..)
 import Html.Events exposing (..)
 import Array exposing (..)
-import Debug exposing (..)
 import Regex exposing (..)
 
 main =
